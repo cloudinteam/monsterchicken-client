df52bab26ca285a2ed79d2f666bef973632a421b
noble-cloudinlabs <noble@cloudinlabs.com>
2023-10-20 16:12:23 +0530

    Monster foods name changed in title
    Monster foods name changed in title

--- src/app/web-pages/about-page/about-page.component.ts
+++ src/app/web-pages/about-page/about-page.component.ts
@@ -16,8 +16,8 @@ export class AboutPageComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.titleService.setTitle('Monster Chicken | Buy Fresh Chicken Online | Best Price Online Raw Chicken');
-    this.metaService.updateTag({ name: 'title', content: 'Monster Chicken | Buy Fresh Chicken Online | Best Price Online Raw Chicken' });
+    this.titleService.setTitle('Monster Foods | Buy Fresh Chicken Online | Best Price Online Raw Chicken');
+    this.metaService.updateTag({ name: 'title', content: 'Monster Foods | Buy Fresh Chicken Online | Best Price Online Raw Chicken' });
     this.metaService.updateTag({ name: 'keywords', content: 'Monsterchicken, chickenbuyonline, freshchicken, best fresh chicken, best fresh chicken online, online chicken, Chicken bulk order, bulk order, buy chicken at namakkal, best chicken in namakkal, quality chicken online, Sri Annapoorna Agro Industries, Omega 3 Enriched Chicken at namakkal, Omega 3 Enriched Egg at namakkal, Omega 3 Enriched Chicken at namakkal, Broiler Chicken at namakkal' });
     this.metaService.updateTag({ name: 'description', content: 'Animal protien is we can take large quantity as per our body requirement. Comparatively vegetable protiens. It will give energy for daily activities and tissue growth.This is the about page description.' });
   }
