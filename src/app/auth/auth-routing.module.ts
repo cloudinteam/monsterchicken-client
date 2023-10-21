bae20f96859929049e78e5272e5d478a99c9e9ce
noble-cloudinlabs <noble@cloudinlabs.com>
2023-10-21 10:48:31 +0530

    logo changed in side bar

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -3,10 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {
-    path: 'login',
-    component: LoginComponent
-  }
+  // {
+  //   path: 'login',
+  //   component: LoginComponent
+  // }
 ];
 
 @NgModule({
